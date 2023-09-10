ca6b4801f161117e68a3b0ff453404eed3bdc449
kumavale <kumavale.hyperion@gmail.com>
2023-09-10 10:38:17 +0900

    style: tweak

--- examples/const.rs
+++ examples/const.rs
@@ -1,13 +1,18 @@
 use match_to_str::match_to_str;
 use rand::Rng;
 
-const ICHIKA:  u8 =  1;
-const NINO:    u8 =  2;
-const MIKU:    u8 =  3;
-const YOTSUBA: u8 =  4;
-const ITSUKI:  u8 =  5;
+#[rustfmt::skip]
+mod nakano {
+    pub const ICHIKA:  u8 = 1;
+    pub const NINO:    u8 = 2;
+    pub const MIKU:    u8 = 3;
+    pub const YOTSUBA: u8 = 4;
+    pub const ITSUKI:  u8 = 5;
+}
 
 fn main() {
+    use nakano::*;
+
     let mut rng = rand::thread_rng();
     let bride = match_to_str!(rng.gen_range(1..=5) => {
         ICHIKA,
